c7fb57f9c0e8407fc975747c2eabc6f6c050bb40
Schafer Gray <schafergray@Schafers-MacBook-Air.local>
2023-11-19 13:47:04 -0500

    Work.

--- src/Connection.ts
+++ src/Connection.ts
@@ -15,14 +15,14 @@ export class Connection {
         // synchronize: true,
         // logging: false,
         type: "postgres",
-         host: "roundhouse.proxy.rlwy.net",
-         port: 21769,
-         username: "postgres",
-         password: "G13dd1fBf-bebb4Fd-1B*-GFf4aBCa2g",
-         database: "railway",
-         entities: [Expense],
-         synchronize: true,
-         logging: false,
+        host: "roundhouse.proxy.rlwy.net",
+        port: 21769,
+        username: "postgres",
+        password: "G13dd1fBf-bebb4Fd-1B*-GFf4aBCa2g",
+        database: "railway",
+        entities: [Expense],
+        synchronize: true,
+        logging: false,
         })
         await client.initialize();
     } catch (error) {
